3c6c8984bc8e90e1544f978936cd33c52532e67d
Hba2019 <36891054+Hba2019@users.noreply.github.com>
2021-12-19 19:53:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ terraform {
   }
 }
 
-resource "aws_instance" "my_new_app_server" {
+resource "aws_instance" "my_new2_app_server" {
   #ami           = "ami-830c94e3"
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
